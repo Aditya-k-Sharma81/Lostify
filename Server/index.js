c0a5a1596c8390c37f95b6301f9b114066ad8143
Aditya Kumar Sharma <adityasharmaas813@gmail.com>
2026-03-13 23:53:31 +0530

    Fix error of cors

--- Server/index.js
+++ Server/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "https://lostify.vercel.app",
+    origin: ["https://lostify.vercel.app", "https://lostify-hazel.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
